2481f8d4206b7c3694fac57380c23a090508e75d
LWH-21 <lwh@free.fr>
2021-11-04 15:37:27 +0100

    simplification and correction of the algorithm

--- calcul_gir.js
+++ calcul_gir.js
@@ -135,185 +135,120 @@ if ((code1 == "A") || (code1 == "B") || (code1 == "C")) {
     correct=0;
 };
 
-console.println("chaine = "+chaine);
-
 
 if (correct==1) {
-
+	// Groupe A
 	groupe=0;
-
-	//Teste pour le groupe A 
-	if (chaine.charAt(0) == "C") {groupe = 2000 ;}
-	if (chaine.charAt(1) == "C") {groupe += 1200 ;}
-	if (chaine.charAt(2) == "C") {groupe += 40 ;}
-	if (chaine.charAt(3) == "C") {groupe += 40 ;}
-	if (chaine.charAt(4) == "C") {groupe += 60 ;}
-	if (chaine.charAt(5) == "C") {groupe += 100 ;}
-	if (chaine.charAt(6) == "C") {groupe += 800 ;}
-	if (chaine.charAt(7) == "C") {groupe += 200 ;}
-	//
-	if (chaine.charAt(2) == "B") {groupe += 16 ;}
-	if (chaine.charAt(3) == "B") {groupe += 16 ;}
-	if (chaine.charAt(4) == "B") {groupe += 20 ;}
-	if (chaine.charAt(5) == "B") {groupe += 16 ;}
-	if (chaine.charAt(6) == "B") {groupe += 120 ;}
-	if (chaine.charAt(7) == "B") {groupe += 32 ;}
-	//Rang groupe A 
-	if (groupe >= 3390 ) {rang = 3 ;}
-	if (groupe >= 4140 ) {rang = 2 ;}
-	if (groupe >= 4380 ) {rang = 1 ;}
-	groupe = 0 ;
-
-	//Teste pour le groupe B
-		if (rang == 0) {
-		if (chaine.charAt(0) == "C") {groupe = 1500 ;}
-			if (chaine.charAt(1) == "C") {groupe += 1200 ;}
-			if (chaine.charAt(2) == "C") {groupe += 40 ;}
-			if (chaine.charAt(3) == "C") {groupe += 40 ;}
-			if (chaine.charAt(4) == "C") {groupe += 60 ;}
-			if (chaine.charAt(5) == "C") {groupe += 100 ;}
-			if (chaine.charAt(6) == "C") {groupe += 800 ;}
-			if (chaine.charAt(7) == "C") {groupe += 80 ;}
-			//
-			if (chaine.charAt(0) == "B") {groupe += 320 ;}
-			if (chaine.charAt(1) == "B") {groupe += 120 ;}
-			if (chaine.charAt(2) == "B") {groupe += 16 ;}
-			if (chaine.charAt(3) == "B") {groupe += 16 ;}
-			if (chaine.charAt(4) == "B") {groupe += 0 ;}
-			if (chaine.charAt(5) == "B") {groupe += 16 ;}
-			if (chaine.charAt(6) == "B") {groupe += 120 ;}
-			if (chaine.charAt(7) == "B") {groupe += - 40 ;}
-			//Rang groupe B
-			if (groupe >= 2016) {rang = 4;}
-		groupe=0;
+	coefs = [2000,1200,40,40,60,100,800,200,0,0];
+	for (i=0;i<10;i++) {
+	  if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
 	}
-
-		//Teste pour le groupe C
-	if (rang == 0) {
-			if (chaine.charAt(2) == "C") {groupe = 40 ;}
-			if (chaine.charAt(3) == "C") {groupe += 40 ;}
-			if (chaine.charAt(4) == "C") {groupe += 60 ;}
-			if (chaine.charAt(5) == "C") {groupe += 160 ;}
-			if (chaine.charAt(6) == "C") {groupe += 1000 ;}
-			if (chaine.charAt(7) == "C") {groupe += 400 ;}
-		//
-			if (chaine.charAt(2) == "B") {groupe += 16 ;}
-			if (chaine.charAt(3) == "B") {groupe += 16 ;}
-			if (chaine.charAt(4) == "B") {groupe += 20 ;}
-			if (chaine.charAt(5) == "B") {groupe += 20 ;}
-			if (chaine.charAt(6) == "B") {groupe += 200 ;}
-			if (chaine.charAt(7) == "B") {groupe += 40 ;}
-			//Rang groupe C	
-			if (groupe >= 1432) {rang = 6;}
-			if (groupe >= 1700) {rang = 5;}
-		groupe=0;
+	coefs = [0,0,16,16,20,16,120,32,0,0];
+	for (i=0;i<10;i++) {
+	  if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
 	}
-
-	//Teste pour le groupe D
-	if (rang == 0) {
-			if (chaine.charAt(4) == "C") {groupe = 2000 ;}
-			if (chaine.charAt(5) == "C") {groupe += 400 ;}
-			if (chaine.charAt(6) == "C") {groupe += 2000 ;}
-			if (chaine.charAt(7) == "C") {groupe += 200 ;}
-		// 
-			if (chaine.charAt(4) == "B") {groupe += 200 ;}
-			if (chaine.charAt(5) == "B") {groupe += 200 ;}
-			if (chaine.charAt(6) == "B") {groupe += 200 ;}
-			//Rang groupe D
-			if (groupe >= 2400) {rang = 7;}
-		groupe=0;
+	if (groupe>=3390) {rang=3;}
+	if (groupe>=4140) {rang=2;}
+	if (groupe>=4380) {rang=1;}
+	// Groupe B
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [1500,1200,40,40,60,100,800,-80,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [320,120,16,16,0,16,120,-40,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=2016) {rang=4;}
 	}
-
-	//Teste pour le groupe E
-	if (rang == 0) {
-			if (chaine.charAt(0) == "C") {groupe = 400 ;}
-			if (chaine.charAt(1) == "C") {groupe += 400 ;}
-		if (chaine.charAt(2) == "C") {groupe += 400 ;}
-			if (chaine.charAt(3) == "C") {groupe += 400 ;}
-			if (chaine.charAt(4) == "C") {groupe += 400 ;}
-			if (chaine.charAt(5) == "C") {groupe += 800 ;}
-			if (chaine.charAt(6) == "C") {groupe += 800 ;}
-			if (chaine.charAt(7) == "C") {groupe += 200 ;}
-		//
-			if (chaine.charAt(2) == "B") {groupe += 100 ;}
-			if (chaine.charAt(3) == "B") {groupe += 100 ;}
-			if (chaine.charAt(4) == "B") {groupe += 100 ;}
-			if (chaine.charAt(5) == "B") {groupe += 100 ;}
-			if (chaine.charAt(6) == "B") {groupe += 100 ;}
-		//Rang groupe E
-			if (groupe >= 1200) {rang = 8;}
-		groupe=0;
+	// Groupe C
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [0,0,40,40,60,160,1000,400,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [0,0,16,16,20,20,200,40,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=1432) {rang=6;}
+	  if (groupe>=1700) {rang=5;}
 	}
-
-	//Teste pour le groupe F
-	if (rang == 0) {
-			if (chaine.charAt(0) == "C") {groupe = 200 ;}
-			if (chaine.charAt(1) == "C") {groupe += 200 ;}
-			if (chaine.charAt(2) == "C") {groupe += 500 ;}
-			if (chaine.charAt(3) == "C") {groupe += 500 ;}
-			if (chaine.charAt(4) == "C") {groupe += 500 ;}
-			if (chaine.charAt(5) == "C") {groupe += 500 ;}
-			if (chaine.charAt(6) == "C") {groupe += 500 ;}
-			if (chaine.charAt(7) == "C") {groupe += 200 ;}
-			//
-			if (chaine.charAt(0) == "B") {groupe += 100 ;}
-			if (chaine.charAt(1) == "B") {groupe += 100 ;}
-			if (chaine.charAt(2) == "B") {groupe += 100 ;}
-			if (chaine.charAt(3) == "B") {groupe += 100 ;}
-			if (chaine.charAt(4) == "B") {groupe += 100 ;}
-			if (chaine.charAt(5) == "B") {groupe += 100 ;}
-			if (chaine.charAt(6) == "B") {groupe += 100 ;}
-			//Rang groupe F
-			if (groupe >= 800) {rang = 9;}
-		groupe=0;
+	// Groupe D
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [0,0,0,0,2000,400,2000,200,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [0,0,0,0,0,200,200,200,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=2400) {rang=7;}
 	}
-
-	//Teste pour le groupe G
-	if (rang == 0) {
-			if (chaine.charAt(0) == "C") {groupe = 150 ;}
-			if (chaine.charAt(1) == "C") {groupe += 150 ;}
-			if (chaine.charAt(2) == "C") {groupe += 300 ;}
-			if (chaine.charAt(3) == "C") {groupe += 300 ;}
-			if (chaine.charAt(4) == "C") {groupe += 500 ;}
-			if (chaine.charAt(5) == "C") {groupe += 500 ;}
-			if (chaine.charAt(6) == "C") {groupe += 400 ;}
-			if (chaine.charAt(7) == "C") {groupe += 200 ;}
-		//
-			if (chaine.charAt(2) == "B") {groupe += 200 ;}
-			if (chaine.charAt(3) == "B") {groupe += 200 ;}
-			if (chaine.charAt(4) == "B") {groupe += 200 ;}
-			if (chaine.charAt(5) == "B") {groupe += 200 ;}
-			if (chaine.charAt(6) == "B") {groupe += 200 ;}
-			if (chaine.charAt(7) == "B") {groupe += 100 ;}
-			//Rang groupe G
-			if (groupe >= 650) {rang = 10;}
-		groupe=0;
+	// Groupe E
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [400,400,400,400,400,800,800,200,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [0,0,100,100,100,100,100,0,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=1200) {rang=8;}
 	}
-
-	//Teste Une Chaine pour le groupe H
-	if (rang == 0) {
-			if (chaine.charAt(2) == "C") {groupe = 3000 ;}
-			if (chaine.charAt(3) == "C") {groupe += 3000 ;}
-			if (chaine.charAt(4) == "C") {groupe += 3000 ;}
-			if (chaine.charAt(5) == "C") {groupe += 3000 ;}
-			if (chaine.charAt(6) == "C") {groupe += 1000 ;}
-			if (chaine.charAt(7) == "C") {groupe += 1000 ;}
-		//
-			if (chaine.charAt(2) == "B") {groupe += 2000 ;}
-			if (chaine.charAt(3) == "B") {groupe += 2000 ;}
-			if (chaine.charAt(4) == "B") {groupe += 2000 ;}
-			if (chaine.charAt(5) == "B") {groupe += 2000 ;}
-			if (chaine.charAt(6) == "B") {groupe += 2000 ;}
-			if (chaine.charAt(7) == "B") {groupe += 1000 ;}
-		//Rang groupe H
-			
-		if (groupe >= 2000) {rang = 12 ;} else {rang = 13 ;}
-			if (groupe >= 4000) {rang = 11 ;}
-			
+	// Groupe F
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [200,200,500,500,500,500,500,200,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [100,100,100,100,100,100,100,0,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=800) {rang=9;}
+	}
+	// Groupe G
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [150,150,300,300,500,500,400,200,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [0,0,200,200,200,200,200,100,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=650) {rang=10;}
+	}
+	// Groupe H
+	if (rang==0) {
+	  groupe=0;
+	  coefs = [0,0,3000,3000,3000,3000,1000,1000,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="C") { groupe += coefs[i]; }
+	  }
+	  coefs = [0,0,2000,2000,2000,2000,2000,1000,0,0];
+	  for (i=0;i<10;i++) {
+		if (chaine.charAt(i)=="B") { groupe += coefs[i]; }
+	  }
+	  if (groupe>=2000) {
+		rang=12;
 		
-		groupe=0;
+	  } else {
+		rang=13;
+	  }
+	  if (groupe>4000) { rang = 11;}
 	}
-	//Girage
+	// Girage
 	if (rang == 13 ) {GIR = 6 ;}
 	if (rang == 12 ) {GIR = 5 ;}
 	if (rang <= 11 ) {GIR = 4 ;}
